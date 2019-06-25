d702fd1bf9e8e45e9c6b02b931416004fece4e55
836745262 <45916600+836745262@users.noreply.github.com>
2019-06-25 13:15:20 +0800

    Add files via upload

---
+++ aroonStrategy/AroonSignal.py
@@ -0,0 +1,30 @@
+import talib as ta
+import numpy as np
+import pandas as pd
+
+"""
+将kdj策略需要用到的信号生成器抽离出来
+"""
+
+class AroonSignal():
+
+    def __init__(self):
+        self.author = 'ChannelCMT'
+
+    def AroonCross(self,am,paraDict):
+        AroonPeriod = paraDict["AroonPeriod"]
+        aroondown, aroonup = ta.AROON(am.high, am.low, timeperiod=AroonPeriod)
+        aroon = aroonup - aroondown
+        goldenCross = aroonup[-1]>paraDict['threshold'] and aroon[-1]>0
+        deathCross = aroondown[-1]<paraDict['threshold'] and aroon[-1]<0
+
+        CrossSignal = 0
+        if goldenCross:
+            CrossSignal = 1
+        elif deathCross:
+            CrossSignal = -1
+        else:
+            CrossSignal = 0
+        return CrossSignal, aroondown, aroonup, aroon
+    
+        
